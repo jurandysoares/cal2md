ce94dc94aacd3cf4f0ff4a1fee2c3b265e7248e5
Jurandy Soares <jurandy.soares@gmail.com>
2020-04-06 13:01:34 -0300

    Improvement on file for todays' annotations

--- hoje.py
+++ hoje.py
@@ -3,13 +3,31 @@
 Open the file of today for annotations
 '''
 
-from datetime import datetime
+import argparse
+from datetime import datetime, timedelta
 from pathlib import Path, PurePath
+import sys
 
-home = Path.home()
-now = datetime.now()
-date = now.strftime("%Y %m %d").split()
-fname = "README.md"
+# Python tutorial: Python Command Line Arguments by 
+# @realpython https://realpython.com/python-command-line-arguments/
 
-path = PurePath(home, *date, fname)
-print(path)
+parser = argparse.ArgumentParser(description='Retorna caminho do arquivo das atividades do dia de hoje')
+parser.add_argument('rel_days', nargs='*')
+args = parser.parse_args()
+
+if not args.rel_days:
+    days_from_today = [0]
+else:
+    days_from_today = map(int, args.rel_days)
+
+fplist = [] # File paths list
+for rel_day in days_from_today:
+    home = Path.home()
+    now = datetime.now()
+    date = now + timedelta(days=rel_day)
+    year,month,day = date.strftime("%Y %m %d").split()
+    fname = "README.md"
+    path = PurePath(home, year, month, day, fname)
+    fplist.append(path)
+
+print(' '.join([str(p) for p in fplist]))
