63a7db3175b20c2bc80cb20259a90817d52d2072
Jurandy Soares <jurandy.soares@gmail.com>
2020-05-30 21:23:34 -0300

    Use of argparse and pathlib modules

--- TODO.md
+++ TODO.md
@@ -1,3 +1,4 @@
 # TODO
 
 * Make a cross-platform *make_dir_days* function
+* Create argument to include month's name besides month's number
--- cal2md.py
+++ cal2md.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python3
 
-#import argparse
+import argparse
 import calendar
 import datetime
-import os
-from pathlib import Path, PurePath
+import pathlib
 import sys
 
 month_pt = { # Months in Portuguese
@@ -32,31 +31,13 @@ weekdays_pt = {
     6: ('Dom', 'Domingo'),
 }
 
-def parse_args() -> (int, int):
-    num_args = len(sys.argv)
-    if num_args == 1:
-        num_month = datetime.datetime.now().month
-        year = datetime.datetime.now().year
-    elif num_args == 2:
-        num_month = int(sys.argv[1])
-        year = datetime.datetime.now().year
-    elif num_args == 3:
-        num_month = int(sys.argv[1])
-        year = int(sys.argv[2])
-    else:
-        sys.exit(1)
+MONTH_CAL_HEADER = '''
+ Seg | Ter | Qua | Qui | Sex | Sáb | Dom 
+ --- | --- | --- | --- | --- | --- | --- 
+'''
 
-    return year, num_month
+DATE_BODY = '''
 
-def make_dir_days(year, num_month, days):
-    for day in range(1, calendar.monthrange(year, num_month)[1]+1):
-        date_path = f'{year}/{num_month:02d}/{day:02d}'
-        date_pt = f'{weekdays_pt[days[day].weekday()][1]}, {day} de {month_pt[num_month]} de {year}'
-        os.makedirs(date_path, exist_ok=True)
-        day_fname = f'{date_path}/README.md'
-        if not os.path.exists(day_fname): # Doesn't overwrite existing file
-            with open(day_fname, 'w') as dayfile:
-                dayfile.write(f'''# {date_pt}
 
 * [Manhã](#manha)
 * [Tarde](#tarde)
@@ -69,49 +50,97 @@ def make_dir_days(year, num_month, days):
 ## Tarde
 
 ## Noite
-''')
+'''
 
-def year2md(year: int):
-    lines = [f'# Ano de {year}', '']
-    lines.extend([f'* [{month_pt[m]}]({m:02d}/README.md)' for m in range(1, 13)])
-    return '\n'.join(lines)    
+def year2md(year: int, year_dir: pathlib.Path, index: str):
+    year_dir.mkdir()
+    year_index = year_dir / index
+    year_header = f'# Ano de {year}'
+    with year_index.open('w', encoding='utf-8') as year_file:
+        year_file.write(year_header)
+        year_file.write(f'''
 
-def month2md(num_month: int, month: list, year: int) -> str:
-    abr_wkd_pt = [v[0] for _,v in weekdays_pt.items()]
-    table_markup = 7*['---']
-    lines = [f'# {month_pt[num_month]} de {year}', '']
-    lines.extend([
-        '|'+('|'.join(abr_wkd_pt))+'|',
-        '|'+('|'.join(table_markup))+'|'
-        ])
-    for week in month:
-        week_line = [f'[{date.day:>3}]({date.day:02d}/README.md)' if date.month==num_month else '   ' for date in week]
-        lines.append('|'+('|'.join(week_line))+'|')
+||||
+|---|---|---|
+|[Janeiro](01/{index})|[Fevereiro](02/{index})|[Março](03/{index})|
+|[Abril](04/{index})|[Maio](05/{index})|[Junho](06/{index})|
+|[Jullho](07/{index})|[Agosto](08/{index})|[Setembro](09/{index})|
+|[Outubro](10/{index})|[Novembro](11/{index})|[Dezembro](12/{index})|
+    '''
+    )
+    # LIST => 
+    # lines.extend([f'* [{month_pt[m]}]({m:02d}/README.md)' for m in range(1, 13)])
+    # MATRIX =>
 
-    return '\n'.join(lines)
+def day2md(date: datetime.datetime, month_dir: pathlib.Path, index: str):
+    date_dir = month_dir / f'{date.day:02d}'
+    date_dir.mkdir()
+    date_index = date_dir / index
+    year = date.year
+    month = date.month
+    month_name = month_pt[month]
+    day = date.day
+    weekday = date.weekday()
+    weekday_name = weekdays_pt[weekday][1]
+    date_header = f'# {weekday_name}, {day} de {month_name} de {year}'
+    with date_index.open('w', encoding='utf-8') as date_file:
+        date_file.write(date_header)
+        date_file.write(DATE_BODY)            
+
+def month2md(year: int, month: int, year_dir: pathlib.Path, index: str):
+    cal = calendar.Calendar(firstweekday=calendar.MONDAY)
+    month_dir = year_dir / f'{month:02d}'
+    month_dir.mkdir()
+    month_index = month_dir / index
+    month_header = f'# {month_pt[month]} de {year}'
+    with month_index.open('w', encoding='utf-8') as month_file:
+        month_file.write(month_header)
+        month_file.write(MONTH_CAL_HEADER)
+        for di,date in enumerate(cal.itermonthdates(year, month)):
+            cel_content = '   '
+            if month == date.month:
+                day2md(date, month_dir, index)
+                cel_content = f'[{date.day:>3}]({date.day:02d}/{index})'
+            if (di%7) != 6:
+                month_file.write(f'{cel_content}|')
+            else:
+                month_file.write(f'{cel_content}\n')
+
+
+def calendar2markdown(year: int, index:str, path: pathlib.Path):
+    year_dir = path / str(year)
+    if not year_dir.exists():
+        year2md(year, year_dir, index)
+        for month in range(1, 12+1):
+            month2md(year, month, year_dir, index)
 
 def main():
-    #parser = argparse.ArgumentParser(description='Gera calendário em formato Markdown')
-    year, num_month = parse_args()
-    cal = calendar.TextCalendar(firstweekday=calendar.SUNDAY)
-    month = cal.monthdatescalendar (year, num_month)
+    now = datetime.datetime.now()
+    parser = argparse.ArgumentParser(description='Gerador de calendário em formato Markdown')
+    parser.add_argument('year', 
+                        default=now.year, 
+                        help='Year from which you want to build the calendar in Markdown format', 
+                        nargs='?',
+                        type=int)
+    parser.add_argument('-i', '--index',
+                        default='index.md',
+                        help='Index file to be created in each directory. Defaults to <index.md>',
+                        )
+    parser.add_argument('-p', '--path',
+                        default=pathlib.Path.cwd(),
+                        help='Path in which the calendar will be created.'
+                        )
 
-    days = {}
-    for week in month:
-        for date in week:
-            if date.month == num_month:
-                days[date.day] = date
+    args = parser.parse_args()
+    path = pathlib.Path(args.path) if str==type(args.path) else args.path
+    # Check if index is a valide filename
+    index = pathlib.Path(args.index)
+    if index.name != args.index:
+        sys.exit(1)
+    elif index.suffix != '.md':
+        sys.exit(2)
 
-    make_dir_days(year, num_month, days)
-    year_fname = f'{year}/README.md'
-    if not os.path.exists(year_fname):
-        with open(year_fname, 'w') as year_file:
-            year_file.write(year2md(year))    
-    
-    month_fname = f'{year}/{num_month:02d}/README.md'
-    if not os.path.exists(month_fname):
-        with open(month_fname, 'w') as month_file:
-            month_file.write(month2md(num_month, month, year))
+    calendar2markdown(args.year, args.index, path)
 
 if __name__ == '__main__':
     main()
